fe335eb2982d505d6c87b69ad47618d8b36b8103
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-02 19:41:51 +0300

    Add files via upload
    Ödevdeki Adres Defteri Sınıfının kodlarını içerir.

---
+++ AdresSınıfı.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Kisi
+{
+    // Özellikler
+    public string Ad { get; private set; }
+    public string Soyad { get; private set; }
+    public string TelefonNumarasi { get; private set; }
+
+    // Yapıcı Metot
+    public Kisi(string ad, string soyad, string telefonNumarasi)
+    {
+        Ad = ad;
+        Soyad = soyad;
+        TelefonNumarasi = telefonNumarasi;
+    }
+
+    // Kişi Bilgisi Metodu
+    public string KisiBilgisi()
+    {
+        return $"Ad Soyad: {Ad} {Soyad}, Telefon Numarası: {TelefonNumarasi}";
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        // Yeni bir kişi oluştur
+        Kisi kisi = new Kisi("Sena", "Köse", "0505 xxx xxxx");
+
+        // Kişi bilgilerini göster
+        Console.WriteLine(kisi.KisiBilgisi());
+
+        Console.ReadLine();
+    }
+}
